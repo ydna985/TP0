0647d57cc10aae5d852619f7d77956e173176f9e
ydna985 <andy-abdoul-mouhaimine.koutou@polymtl.ca>
2024-09-04 20:15:22 -0400

    to do done

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 import math
 #TODO Affichez l'aire d'un cercle avec le rayon indiqué
 rayon = 4
-
+pie=3.14
+aire=rayon*rayon*pie
 #TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
+print(f"Koutou Andy l'aire du cercle est {aire}")
